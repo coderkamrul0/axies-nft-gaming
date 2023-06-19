bce9678ab1649626bafc09ddbd1af7d260f9c411
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-20 04:21:36 +0600

    Live Auctions Carosoul section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css'
 import Hero from './components/Hero/Hero'
 import CreateAndSell from './components/CreateAndSell/CreateAndSell'
+import LiveAuctions from './components/LiveAuctions/LiveAuctions'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className='max-w-screen-[1920px]'>
       <Hero/>
       <CreateAndSell/>
+      <LiveAuctions/>
     </div>
   )
 }
